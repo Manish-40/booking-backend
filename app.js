97c5bdae8318f68e162c1fd95e1a088c5abcf455
Manish40 <manishsolanki969922@gmail.com>
2026-05-29 18:45:58 +0530

    installed cookie-parser, cors, for connecting with frontend

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
-const express=require("express");
-const pool=require("./config/db")
-const userRouter=require("./routes/userRoutes");
-const movieRouter=require("./routes/movieRoutes");
-const app=express();
+const express = require("express");
+const pool = require("./config/db")
+const userRouter = require("./routes/userRoutes");
+const movieRouter = require("./routes/movieRoutes");
+const app = express();
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+app.use(cookieParser());
+app.use(cors({
+  origin: "http://localhost:5173",
+  credentials: true
+}));
 app.use(express.json());
 
 app.get("/", async (req, res) => {
@@ -36,23 +43,39 @@ app.get("/create-users-table", async (req, res) => {
     res.status(500).send("Error creating users table");
   }
 });
-app.get("/create-movies-table",async(req,res)=>{
-  try
-  {
-  await pool.query(`CREATE TABLE movies (
+app.get("/create-movies-table", async (req, res) => {
+  try {
+    await pool.query(`CREATE TABLE movies (
     id SERIAL PRIMARY KEY,
     title VARCHAR(200),
     image TEXT,
     price INT)`)
-}
-catch(error)
-{
-  console.log(error);
-  res.status(500).send("error creating movies table");
-}
+    res.send("movies table created");
+  }
+  catch (error) {
+    console.log(error);
+    res.status(500).send("error creating movies table");
+  }
 });
-app.use("/",userRouter);
-app.use("/movies",movieRouter);
+app.get("/addmovies", async (req, res) => {
+  try {
+    await pool.query(
+      `INSERT INTO movies(title,image,price) VALUES
+    ('Dhurandhar','Dhurandhar.jpg',250),
+    ('Don','Don.jpg',300),
+    ('Dhoom','Dhoom.jpg',350),
+    ('Avengers','Avengers.jpg',400)`,
+    )
+    res.send("movies added successfully");
+  }
+  catch (error) {
+    console.log(error);
+    res.status(500).send("error in adding movies");
+
+  }
+});
+app.use("/", userRouter);
+app.use("/movies", movieRouter);
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
